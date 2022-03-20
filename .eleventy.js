530afa2f4f4d2ca03f211a6261030a482432d1d8
ryokavertikal <riyosasuke@gmail.com>
2022-03-20 10:11:39 +0700

    upp

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,50 @@
 const { DateTime } = require('luxon');
+const Image = require("@11ty/eleventy-img")
 
 module.exports = function (eleventyConfig) {
     // Copy the `css` directory to the output
     eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy("img");
   
     // Watch the `css` directory for changes
     eleventyConfig.addWatchTarget('css');
+    eleventyConfig.addWatchTarget('img');
 
     eleventyConfig.addFilter('readableDate', (dateObj) => {
       return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
         'dd LLL yyyy'
       );
     });
+
+    // --- START, eleventy-img
+  function imageShortcode(src, alt, sizes="(min-width: 1024px) 100vw, 50vw") {
+    console.log(`Generating image(s) from:  ${src}`)
+    let options = {
+      widths: [600, 900, 1500],
+      formats: ["webp", "jpeg"],
+      urlPath: "/img/",
+      outputDir: "./_site/img/",
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src)
+        const name = path.basename(src, extension)
+        return `${name}-${width}w.${format}`
+      }
+    }
+
+    // generate images
+    Image(src, options)
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    }
+    // get metadata
+    metadata = Image.statsSync(src, options)
+    return Image.generateHTML(metadata, imageAttributes)
+  }
+  eleventyConfig.addShortcode("image", imageShortcode)
+  // --- END, eleventy-img
     
   };
---
+++ posts/spunsugar-jawbreaker-lyrics.md
@@ -0,0 +1,46 @@
+---
+title: Spunsugar - Jawbreaker Lyrics
+description: Spunsugar is an alternative rock band from Sweden with shoegaze influences. 
+tags: post
+date: 2022-03-11
+layout: layouts/post.njk
+---
+
+![spunsugar](https://f4.bcbits.com/img/a1028307145_10.jpg)
+
+I am ready now <br>
+They seem happy to be melting <br>
+All under someone's reign<br>
+
+Hold it steady now<br>
+I'll point where you should be aiming<br>
+It's all worth the pain
+<br>
+Doubting becomes that itch in your gums<br>
+A sudden need to bite<br>
+Your blood and teeth<br>
+Get mixed with the sweet<br>
+And it all turns into spite<br>
+A rock of solid sugar<br>
+<br>
+I can follow him<br>
+With his eyes like a coyote<br>
+That can conjure an earthquake<br>
+
+I will challenge them<br>
+Do everything he told me<br>
+Turn their crosses into snakes<br>
+
+Doubting becomes that itch in your gums<br>
+A sudden need to bite<br>
+Your blood and teeth<br>
+Get mixed with the sweet<br>
+And it all turns into spite<br>
+A rock of solid sugar<br>
+
+Twenty nine<br>
+All is mine<br>
+Neon sign<br>
+Stars align<br>
+
+**[Spunsugar](https://spunsugar.bandcamp.com/)** is an alternative/shoegaze band from Sweden. Their music influences was shoegaze and dreampop. Buy album *Drive through chapel* [Bandcamp](https://spunsugar.bandcamp.com/album/drive-through-chapel).
